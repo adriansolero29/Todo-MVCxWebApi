3114eeaa71493ee13dd9155c35da677da4645df2
adriansolero <adrian.manlangit.solero@gmail.com>
2025-09-24 22:31:21 +0800

    Application Layer

---
+++ Application/UseCases/MemberUseCases/CreateMember.cs
@@ -0,0 +1,27 @@
+﻿using Todo.Entities;
+using Todo.Interfaces.Repositories;
+
+namespace Todo.UseCases.MemberUseCases
+{
+    public class CreateMember
+    {
+        private readonly IMemberRepository memberRepository;
+
+        public CreateMember(IMemberRepository memberRepository)
+        {
+            this.memberRepository = memberRepository;
+        }
+
+        public async Task ExecuteCreateMember(Member obj)
+        {
+            try
+            {
+                await memberRepository.Insert(obj);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
