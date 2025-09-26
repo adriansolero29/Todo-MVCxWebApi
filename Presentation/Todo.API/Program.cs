522e7cde712532c52ee6c97b2306493cbe04d0d5
adriansolero <adrian.manlangit.solero@gmail.com>
2025-09-26 14:46:19 +0800

    Registraton

--- Presentation/Todo.API/Program.cs
+++ Presentation/Todo.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Todo.Interfaces.Repositories;
 using Todo.Persistence.Data;
+using Todo.Persistence.Repositories;
+using Todo.UseCases.MemberUseCases;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<TodoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("InitConnection")));
+
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.AddScoped<CreateMember>();
+builder.Services.AddScoped<GetMembers>();
 
 var app = builder.Build();
 
