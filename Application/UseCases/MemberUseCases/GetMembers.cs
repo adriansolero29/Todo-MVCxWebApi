b864a4115154dfb449c96549ce28b9e5cf3b9ad0
adriansolero <adrian.manlangit.solero@gmail.com>
2025-09-26 14:45:19 +0800

    Usecase for getting members

---
+++ Application/UseCases/MemberUseCases/GetMembers.cs
@@ -0,0 +1,24 @@
+﻿using Todo.Interfaces.Repositories;
+
+namespace Todo.UseCases.MemberUseCases;
+
+public record GetAllMemberResult(long id, string name);
+
+public class GetMembers
+{
+    private readonly IMemberRepository memberRepository;
+
+    public GetMembers(IMemberRepository memberRepository)
+    {
+        this.memberRepository = memberRepository;
+    }
+
+    public async Task<IEnumerable<GetAllMemberResult>> ExecuteAllAsync()
+    {
+        var result = await memberRepository.GetAll();
+
+        return result.Select(x =>
+            new GetAllMemberResult(x.Id, $"{x.First} {x.Last}")
+        );
+    }
+}
