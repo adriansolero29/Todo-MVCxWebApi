421e4623f42c5184c5be17384898fcc98309b564
adriansolero <adrian.manlangit.solero@gmail.com>
2025-10-01 22:11:33 +0800

    Task Use cases

---
+++ Application/UseCases/TaskUseCases/GetTasks.cs
@@ -0,0 +1,33 @@
+﻿using Todo.Interfaces.Repositories;
+
+namespace Todo.UseCases.TaskUseCases;
+
+public record TaskMemberResult(string first, string last);
+public record GetTasksResult(string name, DateTime? dueDate, TaskMemberResult assignedMember);
+
+public class GetTasks
+{
+    private readonly ITaskInfoRepository taskInfoRepository;
+
+    public GetTasks(ITaskInfoRepository taskInfoRepository)
+	{
+        this.taskInfoRepository = taskInfoRepository;
+    }
+
+    public async Task<IEnumerable<GetTasksResult>> ExecuteAllAsync()
+    {
+		try
+		{
+            var result = await taskInfoRepository.GetAll();
+            return result.Select(x => 
+                new GetTasksResult(
+                    x.Name ?? "", 
+                    x.DueDate, 
+                    new TaskMemberResult(x.AssignToMember?.First ?? "", x.AssignToMember?.Last ?? "")));
+		}
+		catch (Exception ex)
+		{
+            throw new Exception("Error occured on get task use case: " + ex.Message);
+		}
+    }
+}
