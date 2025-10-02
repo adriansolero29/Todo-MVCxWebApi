af898750d6d5c62b9f225152f44675c95214988e
adriansolero <adrian.manlangit.solero@gmail.com>
2025-10-02 21:49:19 +0800

    Use case for get one data; Add Id

--- Application/UseCases/TaskUseCases/GetTasks.cs
+++ Application/UseCases/TaskUseCases/GetTasks.cs
@@ -3,7 +3,7 @@
 namespace Todo.UseCases.TaskUseCases;
 
 public record TaskMemberResult(string first, string last);
-public record GetTasksResult(string name, DateTime? dueDate, TaskMemberResult assignedMember);
+public record GetTasksResult(long id, string name, DateTime? dueDate, TaskMemberResult assignedMember);
 
 public class GetTasks
 {
@@ -14,6 +14,24 @@ public class GetTasks
         this.taskInfoRepository = taskInfoRepository;
     }
 
+    public async Task<GetTasksResult?> ExecuteOneAsync(long id)
+    {
+        try
+        {
+            var result = await taskInfoRepository.GetById(id);
+            if (result == null)
+            {
+                return null;
+            }
+
+            return new GetTasksResult(result!.Id, result?.Name ?? "", result?.DueDate, new TaskMemberResult(result?.AssignToMember?.First ?? "", result?.AssignToMember?.Last ?? ""));
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error occured on get task use case: " + ex.Message);
+        }
+    }
+
     public async Task<IEnumerable<GetTasksResult>> ExecuteAllAsync()
     {
 		try
@@ -21,6 +39,7 @@ public class GetTasks
             var result = await taskInfoRepository.GetAll();
             return result.Select(x => 
                 new GetTasksResult(
+                    x.Id,
                     x.Name ?? "", 
                     x.DueDate, 
                     new TaskMemberResult(x.AssignToMember?.First ?? "", x.AssignToMember?.Last ?? "")));
