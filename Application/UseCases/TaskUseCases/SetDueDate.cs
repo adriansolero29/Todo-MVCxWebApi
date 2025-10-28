e7c3e3ad4b8d9d11777ac0166c17ed248f16e24b
adriansolero <adrian.manlangit.solero@gmail.com>
2025-10-28 21:11:11 +0800

    Use case for setting due date

---
+++ Application/UseCases/TaskUseCases/SetDueDate.cs
@@ -0,0 +1,24 @@
+﻿using Todo.Entities;
+using Todo.Interfaces.Repositories;
+
+namespace Todo.UseCases.TaskUseCases;
+
+public class SetDueDate
+{
+    private readonly ITaskInfoRepository taskInfoRepository;
+
+    public SetDueDate(ITaskInfoRepository taskInfoRepository)
+    {
+        this.taskInfoRepository = taskInfoRepository;
+    }
+
+    public async Task Execute(long taskId, DateTime dueDate)
+    {
+        var task = await taskInfoRepository.GetById(taskId);
+        if (task == null) return;    
+
+        task.SetDueDate(dueDate);
+
+        await taskInfoRepository.Update(task);
+    }
+}
