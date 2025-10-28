9ea3c560d95c4d3719b3b88afdf5e72633bdf8a1
adriansolero <adrian.manlangit.solero@gmail.com>
2025-10-28 21:13:05 +0800

    API Controller for subtask

---
+++ Presentation/Todo.API/Controllers/SubTaskController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.API.Models;
+using Todo.UseCases.SubTaskUseCases;
+
+namespace Todo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubTaskController : ControllerBase
+    {
+        private readonly CreateSubTask createSubTask;
+
+        public SubTaskController(CreateSubTask createSubTask)
+        {
+            this.createSubTask = createSubTask;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] SubTaskDTO obj)
+        {
+            try
+            {
+                if (obj == null) return NotFound();
+                await createSubTask.ExecuteAsync(obj.TaskId, obj?.Name ?? "");
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = "Error occured saving task: " + ex.Message });
+            }
+        }
+    }
+}
