0dd4eb514fc178d7b95f42a6fdc6df1d961d0fc8
andreisolero <adrian.manlangit.solero@gmail.com>
2026-01-13 20:24:59 +0800

    Migrations

---
+++ Infrastracture/Todo.Persistence/Migrations/20260113120236_AddCustomPropertyToIdentity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomPropertyToIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "MemberId",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_MemberId",
+                table: "AspNetUsers",
+                column: "MemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Members_MemberId",
+                table: "AspNetUsers",
+                column: "MemberId",
+                principalTable: "Members",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Members_MemberId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_MemberId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MemberId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Infrastracture/Todo.Persistence/Migrations/20260113120843_SetMemberIdNullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetMemberIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Members_MemberId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "MemberId",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Members_MemberId",
+                table: "AspNetUsers",
+                column: "MemberId",
+                principalTable: "Members",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Members_MemberId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "MemberId",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Members_MemberId",
+                table: "AspNetUsers",
+                column: "MemberId",
+                principalTable: "Members",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
