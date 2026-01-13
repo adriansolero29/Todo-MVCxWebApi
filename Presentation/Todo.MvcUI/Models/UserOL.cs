f15b0f9c74b28fe9eb6c1d135db6d8c9ecc99cf7
andreisolero <adrian.manlangit.solero@gmail.com>
2026-01-13 20:29:35 +0800

    MVC Model for User

---
+++ Presentation/Todo.MvcUI/Models/UserOL.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.MvcUI.Models
+{
+    public class UserOL
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public required MemberOL MemberInfo { get; set; }
+    }
+}
