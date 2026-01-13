0a1db3e4587862d101b40602f9ac0754388d1a3a
andreisolero <adrian.manlangit.solero@gmail.com>
2026-01-13 20:28:06 +0800

    Initial setup for Identity

--- Infrastracture/Todo.Persistence/Data/TodoDbContext.cs
+++ Infrastracture/Todo.Persistence/Data/TodoDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Todo.Persistence.Entities;
 
 namespace Todo.Persistence.Data;
 
-public class TodoDbContext : DbContext
+public class TodoDbContext : IdentityDbContext<UserEntity>
 {
     public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
     {
--- Presentation/Todo.API/Program.cs
+++ Presentation/Todo.API/Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Todo.Interfaces.Repositories;
 using Todo.Persistence.Data;
+using Todo.Persistence.Entities;
 using Todo.Persistence.Repositories;
 using Todo.UseCases.MemberUseCases;
 using Todo.UseCases.SubTaskUseCases;
 using Todo.UseCases.TaskUseCases;
+using Todo.UseCases.UserUseCases;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +20,16 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<TodoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("InitConnection")));
 
+builder.Services.AddIdentity<UserEntity, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<TodoDbContext>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddScoped<ITaskInfoRepository, TaskRepository>();
 builder.Services.AddScoped<ISubTaskInfoRepository, SubTaskRepository>();
@@ -26,6 +39,7 @@ builder.Services.AddScoped<CreateTask>();
 builder.Services.AddScoped<GetTasks>();
 builder.Services.AddScoped<CreateSubTask>();
 builder.Services.AddScoped<SetDueDate>();
+builder.Services.AddScoped<CreateUser>();
 
 var app = builder.Build();
 
