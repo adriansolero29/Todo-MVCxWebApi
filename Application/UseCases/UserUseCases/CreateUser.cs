69710d559f653f23c57c6f086949a6f45618266b
andreisolero <adrian.manlangit.solero@gmail.com>
2026-01-13 20:28:31 +0800

    Use case for creating user

---
+++ Application/UseCases/UserUseCases/CreateUser.cs
@@ -0,0 +1,28 @@
+﻿using Todo.Entities;
+using Todo.Interfaces.Repositories;
+
+namespace Todo.UseCases.UserUseCases
+{
+    public class CreateUser
+    {
+        private readonly IUserRepository userRepository;
+
+        public CreateUser(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task ExecuteAsync(string username, string password)
+        {
+            try
+            {
+                var toSave = new UserInfo(username, password);
+                await userRepository.Insert(toSave);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Use case error" + ex.Message);
+            }
+        }
+    }
+}
